c49f1fc66d426b9531cf60e09b9f469f5576be31
WeatherBoy <dafloxi@gmail.com>
2025-09-18 18:54:48 +0200

    :goal_net: ♻️ string_handling.py - simpler error handling
    In short, this way if the optional parameter `response_key` for the error message has been left blank, then there will simply be a double space in the error message.
    It isn't the MOST elegant fix, but it saves a lot of boilerplate. PLUS, now I think it is easier to read.

--- infographic/utils/string_handling.py
+++ infographic/utils/string_handling.py
@@ -20,6 +20,8 @@ def valid_and_digit(
 
     :return: The response converted to an int.
     """
+    if response_key is None:
+        response_key = ""
 
     if isinstance(json_response, int):
         int_response = int(json_response)
@@ -52,6 +54,9 @@ def split_the_difference(json_response: Any, response_key: str | None) -> float:
 
     :return: The response converted to a float.
     """
+    if response_key is None:
+        response_key = ""
+
     if isinstance(json_response, (int, float)):
         float_response = float(json_response)
     elif isinstance(json_response, str) and "-" in json_response:
@@ -60,9 +65,6 @@ def split_the_difference(json_response: Any, response_key: str | None) -> float:
         high = float(parts[1])
         float_response = (low + high) / 2.0  # <-- split the difference
     else:
-        if response_key:
-            raise ValueError(f"Invalid {response_key} value: {json_response!r}")
-        else:
-            raise ValueError(f"Invalid value: {json_response!r}")
+        raise ValueError(f"Invalid {response_key} value: {json_response!r}")
 
     return float_response
