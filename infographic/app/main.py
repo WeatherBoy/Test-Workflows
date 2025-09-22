d860345c94a46fa4affa4af26847b00876d3cf40
WeatherBoy <dafloxi@gmail.com>
2025-09-22 13:40:16 +0200

    ✨ main.py - kinda works now

--- infographic/app/main.py
+++ infographic/app/main.py
@@ -1,16 +1,14 @@
-from utils.loader import load_patient_responses
+from utils.loader import load_instruments, load_patient_responses
 from utils.preprocess import split_answers_and_comments
+from utils.process import process_responses
+from visualisation.dashboards import dashboard_v1a, dashboard_v1b
 
 PATIENT = "P001"
 
 if __name__ == "__main__":
     responses = load_patient_responses(PATIENT)
     answers, comments = split_answers_and_comments(responses)
-
-    print("responses:")
-    print(responses)
-
-    print("Answers:")
-    print(answers)
-    print("\nComments:")
-    print(comments)
+    responses = process_responses(answers, comments)
+    instruments = load_instruments()
+    dashboard_v1a(PATIENT, instruments, responses)
+    dashboard_v1b(PATIENT, instruments, responses)
